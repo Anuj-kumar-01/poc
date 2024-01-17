5a830ae6b146c8467d156b0598eff892b6c20f40
Anuj-kumar-01 <anuj.kumar@beamstacks.com>
2024-01-17 17:11:37 +0530

    patch: alignment was corrected

--- src/pages/CkEditor.jsx
+++ src/pages/CkEditor.jsx
@@ -205,7 +205,7 @@ function CkEditor({ editorContent, setEditorContent }) {
           }}
         />
         <button
-          className="font-semibold mt-6 text-white rounded px-6 py-2 ml-2 bg-[#005A84]"
+          className="font-semibold mt-6 text-white rounded px-6 py-2 mr-2 bg-[#005A84]"
           onClick={manipulateHtmlContent}
         >
           Apply rules
